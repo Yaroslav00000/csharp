ce59a2de0170ab71ea84412e1ee1333bd71124a9
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-11-14 23:00:34 +0200

    task complited

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
-﻿namespace csharp
+﻿using System.Text;
+
+namespace csharp
 {
     internal class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Введіть перше число:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введіть друге число більше за перше:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            for (int i = a; i <= b; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(i+ " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
