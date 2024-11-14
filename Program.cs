307567ee751daf12f951a673f90c7c371cb9d4ac
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-11-14 22:40:53 +0200

    task complited

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿namespace csharp
+﻿using System.Text;
+
+namespace csharp
 {
     internal class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.Write("Введіть шестизначне число: ");
+            string? input = Console.ReadLine();
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            string reversedString = new string(charArray);
+            int reversedNumber = int.Parse(reversedString);
+            Console.WriteLine($"Перевернуте число: {reversedNumber}");
         }
     }
 }
