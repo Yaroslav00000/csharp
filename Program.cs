f10fff0011c3ac564c2c7e814f2eb6c109dc01eb
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-11-14 22:54:54 +0200

    task complited

--- Program.cs
+++ Program.cs
@@ -1,10 +1,34 @@
-﻿namespace csharp
+﻿using System.Text;
+
+namespace csharp
 {
     internal class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Верхня границя інтервалу: ");
+            int start = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Нижня границя інтервалу: ");
+            int end = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Числа Фібоначчі в діапазоні від {start} до {end}:");
+            int? a = 0;
+            int? b = 1;
+            for (int i = 0; i < end; i++)
+            {
+                if (a >= start)
+                {
+                    Console.WriteLine(a);
+                }
+                int? c = a + b;
+                a = b;
+                b = c;
+                if (a > end)
+                {
+                    break;
+                }
+            }
         }
     }
 }
